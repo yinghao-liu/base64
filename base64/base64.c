7e47fee3e539d164035314f789a113737e426a21
yinghao-liu <yinghao_liu@163.com>
2017-03-18 17:34:08 +0800

    decoder is done

--- base64/base64.c
+++ base64/base64.c
@@ -14,18 +14,9 @@
  *
  */
 #include <stdio.h>
-
 static const unsigned char map_table[64]={"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"};
-
-/*
- ********************************************
- *
- *
- *
- *
- ********************************************
- */
-char *base64_encoding(char *dest, char const *src)
+/******************************* encoding ********************************/
+char *base64_encode(char *dest, char const *src)
 {
 	unsigned char index;
 	if (*dest !='\0' || src == NULL || dest == NULL){
@@ -51,29 +42,43 @@ char *base64_encoding(char *dest, char const *src)
 		index = *src++ & 0x3f;
 		*dest++ = map_table[index];
 	}
-	//printf("%s\n",dest);
 	return dest;
 }
-/*
- ********************************************
- *
- *
- *
- *
- ********************************************
- */
-char *base64_decoding(char const *src)
-{
-	return NULL;
-}
 
 
-int main(void)
+/**************************** decoding ***********************************/
+char *base64_decode(char *dest, char const *src)
 {
-	char a[]="A";
-	char aa[]="AB";
-	char aaa[]="WWW.BAIDU.COM";
-	base64_encoding(a);
-	base64_encoding(aa);
-	printf("%s\n",base64_encoding(aaa));
+	unsigned char index = 0;	
+	unsigned char tmp[4] = {0};	
+	char *dst_tmp = dest;
+
+	if (*dest !='\0' || src == NULL || dest == NULL){
+		return NULL;
+	}
+	while (*src != '\0'){
+		/*there is no need to handle '=', because tmp is always 0 if there is no match below*/
+		if (*src == '+'){
+			tmp[index] = 62;
+		}else if (*src == '/'){
+			tmp[index] = 63;
+		}else if ((*src >= '0') && (*src <= '9') ){/*'0' is 48 in ascii while 52 in base64*/
+			tmp[index] = *src + 4;
+		}else if ((*src >= 'A') && (*src <= 'Z') ){/*'A' is 65 in ascii while 0 in base64*/
+			tmp[index] = *src - 'A';
+		}else if ((*src >= 'a') && (*src <= 'z') ){/*'a' is 97 in ascii while 26 in base64*/
+			tmp[index] = *src - 71;
+		}
+
+		src++;
+		index++;
+		if (index >= 4 || *src == '\0'){
+			*dst_tmp++ = tmp[0]<<2 | tmp[1]>>4;
+			*dst_tmp++ = tmp[1]<<4 | tmp[2]>>2;
+			*dst_tmp++ = tmp[2]<<6 | tmp[3];
+			tmp[0]=tmp[1]=tmp[2]=tmp[3]=0; /*if there is not enough '=' in the end, it's doesn't matter*/
+		}
+	}
+	return dest;
 }
+
